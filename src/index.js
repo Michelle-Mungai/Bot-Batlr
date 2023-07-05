d7aef8438cb1812aa58fc3ecba62bf59dd7f7a78
Michelle-Mungai <chellymungai@gmail.com>
2023-07-05 20:40:50 +0300

    deployment amends

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8000;
+
+server.use(middlewares);
+server.use(router)
+
+server.listen(port)
+
 root.render(
   <React.StrictMode>
     <App />
